23902c704230fa3298b6c96b82d436566f5eccc7
Thomas Ryan <DWTrip@gmail.com>
2024-10-18 15:53:54 +0900

    Initial commit. Basic app structure in place.

---
+++ server/src/server.js
@@ -0,0 +1,17 @@
+const http = require("http");
+const app = require("./app");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 7007;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  if (PORT) {
+    console.log(`Server launched.\nListening on port: ${PORT}`);
+  } else {
+    console.error(
+      "Failed to use port number given. It is likely that either the port inputed was an invalid data type or that port was not properly defined"
+    );
+  }
+});
